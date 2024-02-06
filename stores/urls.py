4b223a270ef4c999b5b6f19f219786166e644b6e
AnggiAnggianaSujana <anggiana0092@gmail.com>
2024-02-07 00:14:54 +0700

    feat: add product description in specific page for each product

--- stores/urls.py
+++ stores/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns  = [
     path('', views.homepage, name='homepage'),
+    path('products/<product_id>', views.product_show, name='product_show'),
 ]
--- stores/views.py
+++ stores/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from stores.models import Products, Sellers
+from stores.models import Products
 import locale
 
 def homepage(request):
@@ -9,4 +9,11 @@ def homepage(request):
     
     return render(request, 'stores/homepage.html', {
         'smartphone': smartphone,
+    })
+    
+def product_show(request, product_id):
+    list_product = Products.objects.get(pk=product_id)
+    
+    return render(request, 'stores/product_list.html', {
+        'list_product': list_product,
     })
